5f448f7c8697273964b6c317b90ee697bb219142
Brett Sutton <bsutton@noojee.com.au>
2022-08-29 17:37:44 +1000

    Add existing project files to Git

---
+++ bin/dshell.dart
@@ -0,0 +1,61 @@
+#! /usr/bin/env dcli
+
+import 'dart:io';
+
+import 'package:dcli/dcli.dart';
+
+void main(List<String> args) {
+  // Loop, asking for user input and evaluating it
+  for (;;) {
+    // print a > as a prompt
+    stdout.write('${green(basename(pwd))}${blue('>')}');
+    final line = stdin.readLineSync() ?? '';
+    if (line.isNotEmpty) {
+      evaluate(line);
+    }
+  }
+}
+
+// Evaluate the users input
+void evaluate(String command) {
+  final parts = command.split(' ');
+  switch (parts[0]) {
+    // list files in the current directory
+    case 'ls':
+      ls(parts.sublist(1));
+      break;
+
+    // change directories
+    case 'cd':
+      Directory.current = join(pwd, parts[1]);
+      break;
+
+    // treat the first word as the name of an app
+    // and run it.
+    default:
+      if (which(parts[0]).found) {
+        command.run;
+      } else {
+        print(red('Unknown command: ${parts[0]}'));
+      }
+      break;
+  }
+}
+
+/// our own implementation of the 'ls' command.
+void ls(List<String> patterns) {
+  if (patterns.isEmpty) {
+    find('*',
+        workingDirectory: pwd,
+        recursive: false,
+        types: [Find.file, Find.directory]).forEach((file) => print('  $file'));
+  } else {
+    for (final pattern in patterns) {
+      find(pattern,
+              workingDirectory: pwd,
+              recursive: false,
+              types: [Find.file, Find.directory])
+          .forEach((file) => print('  $file'));
+    }
+  }
+}
